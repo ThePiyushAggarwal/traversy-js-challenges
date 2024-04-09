340bd20d51124514090225ee8806200919e8d973
Piyush Aggarwal <web.piyushaggarwal@gmail.com>
2024-04-09 21:53:35 +0530

    Update find-max-number.js

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,13 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let largest = arr[0];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+
+  return largest;
+}
 
 module.exports = findMaxNumber;
