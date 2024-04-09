635358db9101cb16c93105ae0f33189b948876a2
Piyush Aggarwal <web.piyushaggarwal@gmail.com>
2024-04-09 22:28:11 +0530

    Update title-case.js

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,8 @@
-function titleCase() {}
+function titleCase(sentence) {
+  return sentence
+    .split(' ')
+    .map((str) => str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase())
+    .join(' ');
+}
 
 module.exports = titleCase;
