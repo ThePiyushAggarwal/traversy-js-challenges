0ea6a88a115ffc18c8ab80f34f0b52b67da5f36b
Piyush Aggarwal <web.piyushaggarwal@gmail.com>
2024-04-09 21:18:39 +0530

    Update calculator.js

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,17 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num1 / num2;
+    default:
+      // If the operator is not one of the above, throw an error
+      throw new Error('Invalid operator');
+  }
+}
 
 module.exports = calculator;
