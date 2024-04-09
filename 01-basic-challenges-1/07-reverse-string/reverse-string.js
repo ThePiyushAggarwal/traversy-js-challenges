2074f7d68c991b44ddf0af17329848bccc711471
Piyush Aggarwal <web.piyushaggarwal@gmail.com>
2024-04-09 22:45:59 +0530

    Update reverse-string.js

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+  let final = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    final += str[i];
+  }
+  return final;
+}
 
 module.exports = reverseString;
