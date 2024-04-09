839d9001538b420a7d6e866b5a775d267330524c
Piyush Aggarwal <web.piyushaggarwal@gmail.com>
2024-04-10 00:40:16 +0530

    Update palindrome.js

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,6 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const formattedStr = str.toLowerCase().replaceAll(/[^a-z0-9]/g, '');
+  return formattedStr === formattedStr.split('').reverse().join('');
+}
 
 module.exports = isPalindrome;
