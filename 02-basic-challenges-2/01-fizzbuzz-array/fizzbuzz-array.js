4dd4eea9582b1d547c6dea1512e8294ca3f73f0b
Piyush Aggarwal <web.piyushaggarwal@gmail.com>
2024-04-13 23:15:53 +0530

    Update fizzbuzz-array.js

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,18 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(length) {
+  const arr = Array.from({ length }, (_item, i) => i + 1);
+  return arr.map((item) => {
+    const divisibleBy3 = item % 3 === 0;
+    const divisibleBy5 = item % 5 === 0;
+    const divisibleByBoth = divisibleBy3 && divisibleBy5; // Can be checked for divisible by 15
+
+    return divisibleByBoth
+      ? 'FizzBuzz'
+      : divisibleBy5
+      ? 'Buzz'
+      : divisibleBy3
+      ? 'Fizz'
+      : item;
+  });
+}
 
 module.exports = fizzBuzzArray;
