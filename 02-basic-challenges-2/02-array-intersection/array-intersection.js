bb05100caa44a39bf9bf10a93e6c524a150a3b99
Piyush Aggarwal <piyush.aggarwal@taskex.in>
2024-04-15 22:16:08 +0530

    Update array-intersection.js

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,14 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const resultingArray = [];
+
+  for (const element of arr2) {
+    if (set1.has(element)) {
+      resultingArray.push(element);
+    }
+  }
+
+  return resultingArray;
+}
 
 module.exports = arrayIntersection;
